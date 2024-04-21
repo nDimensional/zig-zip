bcd3ae70835c65085cb0299ffbeda970426dfc3e
Jonathan Marler <johnnymarler@gmail.com>
2024-04-20 19:02:24 -0600

    zig update 0.12.0
    use std.Build.path instead of LazyPath.relative

--- build.zig
+++ build.zig
@@ -1,11 +1,10 @@
 const std = @import("std");
-const LazyPath = std.Build.LazyPath;
 
 pub fn build(b: *std.Build) void {
-    const zip = b.addModule("zip", .{ .root_source_file = LazyPath.relative("src/zip.zig") });
+    const zip = b.addModule("zip", .{ .root_source_file = b.path("src/zip.zig") });
 
     // Tests
-    const tests = b.addTest(.{ .root_source_file = LazyPath.relative("src/zip_test.zig") });
+    const tests = b.addTest(.{ .root_source_file = b.path("src/zip_test.zig") });
     tests.root_module.addImport("zip", zip);
     const run_tests = b.addRunArtifact(tests);
 
